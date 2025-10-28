d178e913719847a9bbf62363d2217dac475bdb0a
Neo <138670176+Oneo64@users.noreply.github.com>
2025-10-28 17:01:15 +0800

    Update declensions.js

--- js/declensions.js
+++ js/declensions.js
@@ -471,8 +471,6 @@ function get_present_tense(word, pov) {
 			return stem.substring(0, stem.length - 1) + "ý";
 		} else if (stem.endsWith("á")) {
 			return stem.substring(0, stem.length - 1) + "æ";
-		} else if (stem.endsWith("eyj")) {
-			return stem.substring(0, stem.length - 1);
 		} else if (stem.endsWith("j")) {
 			return stem.substring(0, stem.length - 1) + "i";
 		} else {
