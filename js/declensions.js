185fd372b9b28f3520842bc254613def01c040a6
Neo <138670176+Oneo64@users.noreply.github.com>
2025-12-23 20:31:10 +0800

    d

--- js/declensions.js
+++ js/declensions.js
@@ -676,14 +676,16 @@ function get_present_tense(word, pov) {
 			return word;
 		}
 	} else if (pov == 2) {
-		if (stem.endsWith("ú")) {
+		if ((stem.endsWith("f") || stem.endsWith("r") || stem.endsWith("g") || stem.endsWith("t")) && !stem.endsWith("tt")) {
+			return stem + "i";
+		} else if (stem.endsWith("ú")) {
 			return stem.substring(0, stem.length - 1) + "ý";
 		} else if (stem.endsWith("á")) {
 			return stem.substring(0, stem.length - 1) + "æ";
 		} else if (stem.endsWith("j")) {
 			return stem.substring(0, stem.length - 1) + "i";
 		} else {
-			return stem + "i";
+			return stem + "ar";
 		}
 	} else {
 		if ((stem.endsWith("f") || stem.endsWith("r") || stem.endsWith("g") || stem.endsWith("t")) && !stem.endsWith("tt")) {
