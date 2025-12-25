400286fc9d1196227e104734735359a111879df0
Neo <138670176+Oneo64@users.noreply.github.com>
2025-12-25 17:04:42 +0800

    Update oddarish_dictionary.js

--- js/oddarish_dictionary.js
+++ js/oddarish_dictionary.js
@@ -3436,7 +3436,7 @@ const entries = {
 	"hrungr": ["adjective", "brown", "fictional"],
 	"hrákk": ["neuter noun", "endurance, bodily strength, constitution, resilience; resistance, armor, protection", "fictional"],
 	"hrákk-ugr": ["adjective", "athletic, acrobatic, gymnastic"],
-	"hrákkja": ["verb", "to resist, to withstand, to endure, to tolerate; (with \"frá\") to resist, to endure; a battlecry shouted by warriors", "fictional"],
+	"hrákkja": ["verb", "(with \"frá\") to resist, to withstand, to endure; to endure, to stand firm, to hold together; to brave, to overcome", "fictional"],
 	"hrápna": ["verb", "to attack with an army", "fictional"],
 	"hrápning-fekla": ["feminine noun", "artillery, siege weapons"],
 	"hrása": ["verb", "to tightly hug or embrace; to tightly hold someone or soemthing", "fictional"],
