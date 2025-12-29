9fa3762e95c8fda1726cbf6e2567306f24d0eb40
Neo <138670176+Oneo64@users.noreply.github.com>
2025-12-29 13:32:37 +0800

    Update oddarish_dictionary.js

--- js/oddarish_dictionary.js
+++ js/oddarish_dictionary.js
@@ -5551,7 +5551,7 @@ const entries = {
 	"tefning": ["feminine noun", "design, appearance", "fictional"],
 	"tegja": ["verb", "to shape, to form, to create", "fictional"],
 	"teiginn": ["adjective", "simple, basic, common", "fictional"],
-	"tekja": ["verb", "to take, to bring; to take into possession; to take something along, to bring someone along (with accusative + \"hið mir/þir/sir/því\")", "old-norse evolved", ["Getrðu tekja mir tólit? = Can you bring me the tool?", "Hann takti sér bástann. = He took a hat with him.", "Þose tekju mek hið þir til lytingina. = Please take me with you to the festival."]],
+	"tekja": ["verb", "to take, to bring; to take into possession; to take something along, to bring someone along (with accusative + \"með mir/þir/sir/því\")", "old-norse evolved", ["Getrðu tekja mir tólit? = Can you bring me the tool?", "Hann takti sér bástann. = He took a hat with him.", "Þose tekju mek hið þir til lytingina. = Please take me with you to the festival."]],
 	"tellr": ["masculine noun", "speech", "fictional"],
 	"tenda": ["feminine noun", "feeling, emotion; mood; sense", "fictional"],
 	"tenda-stötvunugr": ["adjective", "emotionally stable or consistent"],
